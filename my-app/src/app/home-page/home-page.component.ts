b964758d90bea82fed349deb7b2d3321f78c9ee2
Nalin Chopra <nalin.chopra@berkeley.edu>
2018-12-23 18:41:58 -0800

    added icons and jquery functionality

--- my-app/src/app/home-page/home-page.component.ts
+++ my-app/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
+    $('#scroller').click(function(e) {
+      $('html, body').animate({ scrollTop: $('.mainDescription').offset().top }, 1000);
+    });
   }
-
 }
